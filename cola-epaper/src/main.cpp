38c687c86513cbb5ea780a0be3755ffcd4fca20d
ggkk0818 <ggkk0818@sina.com>
2026-04-22 00:43:00 +0800

    调整针脚定义

--- cola-epaper/src/main.cpp
+++ cola-epaper/src/main.cpp
@@ -3,12 +3,12 @@
 #include <GxEPD2_3C.h>
 #include <Fonts/FreeMonoBold24pt7b.h>
 
-static constexpr uint8_t EPD_SCK_PIN = 13;
-static constexpr uint8_t EPD_MOSI_PIN = 14;
-static constexpr uint8_t EPD_CS_PIN = 15;
-static constexpr uint8_t EPD_RST_PIN = 26;
-static constexpr uint8_t EPD_DC_PIN = 27;
-static constexpr uint8_t EPD_BUSY_PIN = 25;
+static constexpr uint8_t EPD_SCK_PIN = 12;
+static constexpr uint8_t EPD_MOSI_PIN = 13;
+static constexpr uint8_t EPD_CS_PIN = 10;
+static constexpr uint8_t EPD_RST_PIN = 14;
+static constexpr uint8_t EPD_DC_PIN = 9;
+static constexpr uint8_t EPD_BUSY_PIN = 11;
 
 // Waveshare 4.2" 3-color (B/C) panel: 400x300
 GxEPD2_3C<GxEPD2_420c, GxEPD2_420c::HEIGHT> display(
@@ -18,9 +18,9 @@ GxEPD2_3C<GxEPD2_420c, GxEPD2_420c::HEIGHT> display(
 void setup() {
   Serial.begin(115200);
 
-  // Match SPI pins from DEV_Config.h: SCK=13, MOSI=14, CS=15
-  // SPI.begin(EPD_SCK_PIN, -1, EPD_MOSI_PIN, EPD_CS_PIN);
-  // display.epd2.selectSPI(SPI, SPISettings(4000000, MSBFIRST, SPI_MODE0));
+  // Match SPI pins from DEV_Config.h: SCK=12, MOSI=13, CS=10
+  SPI.begin(EPD_SCK_PIN, -1, EPD_MOSI_PIN, EPD_CS_PIN);
+  display.epd2.selectSPI(SPI, SPISettings(4000000, MSBFIRST, SPI_MODE0));
 
   display.init(115200);
   display.setRotation(0);
