5ee2ffc98e01b2820a5024f49c58177c74c17781
ggkk0818 <ggkk0818@sina.com>
2026-05-26 23:05:14 +0800

    增加测试代码

--- cola-epaper-i7/src/main.cpp
+++ cola-epaper-i7/src/main.cpp
@@ -1,13 +1,28 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
 #include "modules/display/DisplayModule.h"
 
+namespace {
+constexpr uint8_t RGB_LED_PIN = 27;
+constexpr uint8_t RGB_LED_COUNT = 1;
+}
+
 DisplayModule displayModule;
+Adafruit_NeoPixel rgbLed(RGB_LED_COUNT, RGB_LED_PIN, NEO_RGB + NEO_KHZ800);
 
 void setup() {
   displayModule.begin();
   displayModule.renderLogo();
   displayModule.hibernate();
+
+  rgbLed.begin();
+  rgbLed.clear();
+  rgbLed.setPixelColor(0, rgbLed.Color(55, 255, 55));
+  rgbLed.show();
+  delay(1000);
+  rgbLed.clear();
+  rgbLed.show();
 }
 
 void loop() {
