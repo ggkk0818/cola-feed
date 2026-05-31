0049ea214bf8bb663ecef371e0bcd421b5197918
ggkk0818 <ggkk0818@sina.com>
2026-05-31 20:22:15 +0800

    增加天气图标

---
+++ cola-epaper-i7/include/modules/display/weather-icons/401.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Arduino.h>
+
+namespace WeatherIcon401 {
+constexpr uint16_t kWidth = 32;
+constexpr uint16_t kHeight = 32;
+constexpr size_t kByteSize = (kWidth * kHeight) / 8;
+
+extern const uint8_t kBitmap[kByteSize];
+}  // namespace WeatherIcon401
