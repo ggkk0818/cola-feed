3557eb14df854fceb966aaa5abf7db0dda3cb065
ggkk0818 <ggkk0818@sina.com>
2026-05-31 20:42:16 +0800

    天气图标改为64x64大小

--- cola-epaper-i7/include/modules/display/weather-icons/511.h
+++ cola-epaper-i7/include/modules/display/weather-icons/511.h
@@ -3,8 +3,8 @@
 #include <Arduino.h>
 
 namespace WeatherIcon511 {
-constexpr uint16_t kWidth = 32;
-constexpr uint16_t kHeight = 32;
+constexpr uint16_t kWidth = 64;
+constexpr uint16_t kHeight = 64;
 constexpr size_t kByteSize = (kWidth * kHeight) / 8;
 
 extern const uint8_t kBitmap[kByteSize];
