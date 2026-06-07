3005bd13264fd88845ac3d874602f582a5797dfc
ggkk0818 <ggkk0818@sina.com>
2026-06-07 23:04:42 +0800

    调整传感器参数

--- cola-epaper-i7/src/modules/i2c/I2cModule.cpp
+++ cola-epaper-i7/src/modules/i2c/I2cModule.cpp
@@ -42,7 +42,7 @@ constexpr uint8_t kMax17048Address = 0x36;
 constexpr uint8_t kMax17048RegVcell = 0x02;
 constexpr uint8_t kMax17048RegSoc = 0x04;
 
-constexpr uint32_t kAccelerationPollIntervalMs = 200;
+constexpr uint32_t kAccelerationPollIntervalMs = 5000;
 constexpr uint32_t kEnvironmentPollIntervalMs = 60000;
 constexpr uint32_t kBatteryPollIntervalMs = 5000;
 constexpr uint32_t kShtc3MeasurementDelayMs = 15;
